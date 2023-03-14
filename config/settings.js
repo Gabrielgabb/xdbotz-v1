872a58c7b4d1db112d9972d8b9a5fd791ce4c760
Gabrielgabb <126182247+Gabrielgabb@users.noreply.github.com>
2023-03-14 21:00:40 +0300

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const chalk = require('chalk')
 // EDIT DISINI
-global.owner = ['6281220670449'] // no own
-global.author = 'KurrXd' // nama author 
-global.packname = 'xd bot whatsapp' // nama pack sticker
-global.namabot = 'XD BOTZ' // nama bot mu
+global.owner = ['254762065033'] // no own
+global.author = 'Gabby' // nama author 
+global.packname = 'Gabby md' // nama pack sticker
+global.namabot = 'Gabby' // nama bot mu
 global.group = 'https://bit.ly/3ZI51Qk' // grup mu
 global.pic = 'https://telegra.ph/file/b96926809d93d9f045777.jpg' // logo bot
 
